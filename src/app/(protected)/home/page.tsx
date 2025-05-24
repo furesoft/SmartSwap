f80070fdb1687fd572ce4926c025d110ff249be8
Chris Anders <filmee24@gmail.com>
2025-05-24 14:57:32 +0200

    Cleanup

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,11 +1,10 @@
 import { auth } from '@/auth';
 import { Page } from '@/components/PageLayout';
 import { Pay } from '@/components/Pay';
-import { Transaction } from '@/components/Transaction';
-import { UserInfo } from '@/components/UserInfo';
 import { Verify } from '@/components/Verify';
 import { ViewPermissions } from '@/components/ViewPermissions';
 import { Marble, TopBar } from '@worldcoin/mini-apps-ui-kit-react';
+import {UserInfo} from "@/components/UserInfo";
 
 export default async function Home() {
   const session = await auth();
