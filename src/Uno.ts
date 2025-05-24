f5baac00caebc43cea722bddac0707ceeff6f8df
Chris Anders <filmee24@gmail.com>
2025-05-24 21:17:53 +0200

    feat: Use static debug adress

---
+++ src/Uno.ts
@@ -0,0 +1,45 @@
+﻿const UNO_APP_ID = 'app_a4f7f3e62c1de0b9490a5260cb390b56'
+
+
+export function getUnoDeeplinkUrl({
+                               fromToken,
+                               toToken,
+                               amount,
+                               referrerAppId,
+                               referrerDeeplinkPath,
+                           }: {
+    fromToken?: string
+    toToken?: string
+    amount?: string
+    referrerAppId?: string
+    referrerDeeplinkPath?: string
+}) {
+    let path = `?tab=swap`
+    if (fromToken) {
+        path += `&fromToken=${fromToken}`
+        if (amount) {
+            path += `&amount=${amount}`
+        }
+    }
+    if (toToken) {
+        path += `&toToken=${toToken}`
+    }
+    if (referrerAppId) {
+        path += `&referrerAppId=${referrerAppId}`
+    }
+    if (referrerDeeplinkPath) {
+        path += `&referrerDeeplinkPath=${encodeURIComponent(referrerDeeplinkPath)}`
+    }
+    const encodedPath = encodeURIComponent(path)
+    return `https://worldcoin.org/mini-app?app_id=${UNO_APP_ID}&path=${encodedPath}`
+}
+
+export function openUno(fromToken?: string, toToken?: string, amount?: string) {
+    const url = getUnoDeeplinkUrl({
+        fromToken,
+        toToken,
+        amount,
+        process.env.NEXT_PUBLIC_APP_ID
+    })
+    window.open(url)
+}
