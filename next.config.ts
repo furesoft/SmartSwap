6a0937165a0ee605388c7b73c5a243b1ce3c8114
Chris Anders <filmee24@gmail.com>
2025-05-24 11:29:08 +0200

    Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["static.usernames.app-backend.toolsforhumanity.com"],
   },
-  allowedDevOrigins: [
-    "https://7d83-2a02-3102-b930-1ea0-b88d-568c-4ff2-b855.ngrok-free.app/",
-  ], // Add your dev origin here
+  allowedDevOrigins: ["https://smart-swap-nine.vercel.app/"], // Add your dev origin here
   reactStrictMode: false,
 };
 
