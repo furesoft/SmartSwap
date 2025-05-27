5eb868f4984381b4a9c1bc826c3ecc2730e6b4b1
Chris <filmee24@gmail.com>
2025-05-27 14:59:31 +0200

    Update Token.ts

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -3,11 +3,13 @@
     name: string;
     symbol: string;
     decimals: number;
+    claim: boolean;
 
-    constructor(balance: number, name: string, symbol: string, decimals: number) {
+    constructor(balance: number, name: string, symbol: string, decimals: number, claim: boolean) {
         this.balance = balance;
         this.name = name;
         this.symbol = symbol;
         this.decimals = decimals;
+        this.claim = claim;
     }
 }
