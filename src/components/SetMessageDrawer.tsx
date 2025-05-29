3951a94a7a7861320993330f22f557342964c727
Chris Anders <filmee24@gmail.com>
2025-05-29 11:33:57 +0200

    feat: Update SetMessageDrawer to adjust height and improve layout

--- src/components/SetMessageDrawer.tsx
+++ src/components/SetMessageDrawer.tsx
@@ -34,19 +34,22 @@ export default function SetMessageDrawer() {
 
     return (
         <>
-            <Drawer height="full" repositionInputs open={open} onOpenChange={handleDrawerOpen}>
+            <Drawer height="fit" repositionInputs open={open} onOpenChange={handleDrawerOpen}>
                 <DrawerTrigger asChild>
                     <Button variant="secondary" size="sm" onClick={() => handleDrawerOpen(true)}>
                         Set Message
                     </Button>
                 </DrawerTrigger>
+
                 <DrawerContent className="p-6 grow flex flex-col justify-between">
                         <DrawerHeader>
                             <DrawerTitle>Set Message</DrawerTitle>
                         </DrawerHeader>
+
                         <div className="flex flex-col gap-4 pt-4">
                             <Input label="Type here your message" value={input} onChange={e => setInput(e.target.value)} />
                         </div>
+
                         <BottomBar direction="horizontal">
                             <React.Fragment key=".0">
                                     <Button fullWidth variant="secondary" onClick={() => handleDrawerOpen(false)}>
